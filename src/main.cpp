8d638af4faf8d4f46c42ddca24d544f13af86ff9
ruthenium44 <matvey.natsevskiy@gmail.com>
2025-06-10 02:57:26 +0300

    crowdin.yaml

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,74 @@
-﻿// src/main.cpp
-
-#include <QApplication> // Основной класс для управления GUI-приложением
-#include <QSettings>    // Для чтения и записи настроек (например, выбранного языка)
-#include <QTranslator>  // Для загрузки файлов перевода
-#include <QLibraryInfo> // Для получения пути к стандартным переводам Qt
+﻿#include <QApplication>
+#include <QSettings>
+#include <QTranslator>
+#include <QLibraryInfo>
+#include <QDebug>
+#include <QDir>
 
 #include "ui/StartWindow.h"
+#include "core/FileManager.h"
 
 int main(int argc, char *argv[])
 {
-    // 1. Создание экземпляра QApplication.
-    // Это должно быть сделано в первую очередь в любом Qt GUI приложении.
     QApplication app(argc, argv);
 
-    QCoreApplication::setOrganizationName("Borovik Ivan T-391"); // Укажите название вашей "компании"
-    QCoreApplication::setApplicationName("Своя Игра"); // Название вашего приложения
+    QCoreApplication::setOrganizationName("Borovik Ivan T-391");
+    QCoreApplication::setApplicationName("Своя Игра");
 
-    // 3. Логика локализации (переключения языков).
-    QSettings settings;
-    // Читаем сохраненный язык из настроек. Если его нет, по умолчанию будет "ru".
-    QString lang = settings.value("language", "ru").toString();
+    qDebug() << "--- ПАЧАТАК ДЫЯГНОСТЫКІ ЛАКАЛІЗАЦЫІ ---";
 
-    // Загрузчик для наших переводов
-    QTranslator appTranslator;
-    // Загружаем файл перевода в зависимости от выбранного языка.
-    // Файлы .qm должны находиться в подпапке translations.
-    if (appTranslator.load("svojaigra_" + lang, "translations")) {
-        app.installTranslator(&appTranslator);
+    // --- 1. Якую мову мы хочам загрузіць? ---
+    QString lang = FileManager::loadLanguageSetting();
+    qDebug() << "[INFO] Мова, загружаная з налад:" << lang;
+
+    // --- 2. Дзе праграма шукае файлы? ---
+    QString workingDir = QDir::currentPath();
+    qDebug() << "[INFO] Рабочая дырэкторыя праграмы:" << workingDir;
+
+    // --- 3. Правяраем існаванне папкі `translations` ---
+    QString translationsPath = workingDir + "/translations";
+    qDebug() << "[CHECK] Поўны шлях да папкі перакладаў:" << translationsPath;
+
+    QDir translationsDir(translationsPath);
+    if (translationsDir.exists()) {
+        qDebug() << "[OK] Папка 'translations' існуе.";
+    } else {
+        qWarning() << "[ERROR] Папка 'translations' НЕ ІСНУЕ па ўказаным шляху!";
     }
 
-    // Загрузчик для стандартных переводов Qt (кнопки "OK", "Cancel" и т.д.)
+    // --- 4. Правяраем існаванне канкрэтнага файла `.qm` ---
+    QString qmFileName = "svojaigra_" + lang + ".qm";
+    QString fullQmPath = translationsPath + "/" + qmFileName;
+    qDebug() << "[CHECK] Поўны шлях да файла перакладу:" << fullQmPath;
+
+    if (QFile::exists(fullQmPath)) {
+        qDebug() << "[OK] Файл" << qmFileName << "існуе.";
+    } else {
+        qWarning() << "[ERROR] Файл" << qmFileName << "НЕ ЗНОЙДЗЕНЫ па ўказаным шляху!";
+    }
+
+    // --- 5. Спрабуем загрузіць пераклад ---
+    QTranslator appTranslator;
+    // .load() патрабуе назву файла без пашырэння і шлях да папкі
+    if (appTranslator.load("svojaigra_" + lang, translationsPath)) {
+        app.installTranslator(&appTranslator);
+        qDebug() << "[OK] Файл перакладу паспяхова загружаны!";
+    } else {
+        qWarning() << "[ERROR] Спроба загрузкі файла не атрымалася!";
+    }
+
+    qDebug() << "--- КАНЕЦ ДЫЯГНОСТЫКІ ЛАКАЛІЗАЦЫІ ---";
+
+    // Загрузка стандартных перакладаў Qt
     QTranslator qtTranslator;
-    // Путь к стандартным переводам зависит от системы
-    #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-        // Для Qt5
-        if (qtTranslator.load("qt_" + lang, QLibraryInfo::location(QLibraryInfo::TranslationsPath))) {
-            app.installTranslator(&qtTranslator);
-        }
-    #else
-        // Для Qt6
+    #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
         if (qtTranslator.load("qtbase_" + lang, QLibraryInfo::path(QLibraryInfo::TranslationsPath))) {
             app.installTranslator(&qtTranslator);
         }
     #endif
 
-
-    // 4. Создание и отображение главного окна.
-    // В нашем случае это StartWindow, так как с него начинается работа.
     StartWindow startWindow;
-    startWindow.show(); // Показываем окно пользователю
+    startWindow.show();
 
-
-    // 5. Запуск цикла обработки событий.
-    // Программа будет "жить" на этой строке, пока пользователь ее не закроет.
-    // app.exec() вернет код завершения (обычно 0).
     return app.exec();
 }
