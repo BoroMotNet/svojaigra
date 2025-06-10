7e75716882f67c17cb491df133779e60afb4cd05
Matvey Natsevskiy <matvey.natsevskiy@gmail.com>
2025-06-10 03:25:45 +0300

    New translations svojaigra_ru.ts (English)

--- resources/translations/svojaigra_en.ts
+++ resources/translations/svojaigra_en.ts
@@ -1,182 +1,182 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="en" sourcelanguage="ru">
-<context>
+  <context>
     <name>AdminEditor</name>
     <message>
-        <location filename="../../src/ui/AdminEditor.cpp" line="10"/>
-        <source>Редактор вопросов</source>
-        <translation>Question Editor</translation>
+      <location filename="../../src/ui/AdminEditor.cpp" line="10"/>
+      <source>Редактор вопросов</source>
+      <translation>Question Editor</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>GameManager</name>
     <message>
-        <location filename="../../src/core/GameManager.cpp" line="66"/>
-        <source>Не удалось загрузить вопросы. Игра не может быть начата.</source>
-        <translation>Failed to load questions. The game cannot be started.</translation>
+      <location filename="../../src/core/GameManager.cpp" line="66"/>
+      <source>Не удалось загрузить вопросы. Игра не может быть начата.</source>
+      <translation>Failed to load questions. The game cannot be started.</translation>
     </message>
     <message>
-        <location filename="../../src/core/GameManager.cpp" line="92"/>
-        <source>Игрок</source>
-        <translation>Player</translation>
+      <location filename="../../src/core/GameManager.cpp" line="92"/>
+      <source>Игрок</source>
+      <translation>Player</translation>
     </message>
     <message>
-        <location filename="../../src/core/GameManager.cpp" line="93"/>
-        <source>не может выбрать этот вопрос (порог сложности)!</source>
-        <translation>can&apos;t choose this question (difficulty threshold)!</translation>
+      <location filename="../../src/core/GameManager.cpp" line="93"/>
+      <source>не может выбрать этот вопрос (порог сложности)!</source>
+      <translation>can&apos;t choose this question (difficulty threshold)!</translation>
     </message>
     <message>
-        <location filename="../../src/core/GameManager.cpp" line="101"/>
-        <source>Попытка выбрать уже сыгранный вопрос!</source>
-        <translation>Trying to choose a question that has already been played!</translation>
+      <location filename="../../src/core/GameManager.cpp" line="101"/>
+      <source>Попытка выбрать уже сыгранный вопрос!</source>
+      <translation>Trying to choose a question that has already been played!</translation>
     </message>
     <message>
-        <location filename="../../src/core/GameManager.cpp" line="128"/>
-        <source>Порог для игрока</source>
-        <translation>Player threshold</translation>
+      <location filename="../../src/core/GameManager.cpp" line="128"/>
+      <source>Порог для игрока</source>
+      <translation>Player threshold</translation>
     </message>
     <message>
-        <location filename="../../src/core/GameManager.cpp" line="128"/>
-        <source>повышен до</source>
-        <translation>is promoted to</translation>
+      <location filename="../../src/core/GameManager.cpp" line="128"/>
+      <source>повышен до</source>
+      <translation>is promoted to</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../../src/ui/MainWindow.cpp" line="30"/>
-        <source>Своя Игра</source>
-        <translation>Jeopardy</translation>
+      <location filename="../../src/ui/MainWindow.cpp" line="30"/>
+      <source>Своя Игра</source>
+      <translation>Jeopardy</translation>
     </message>
     <message>
-        <location filename="../../src/ui/MainWindow.cpp" line="192"/>
-        <source>Игра окончена!</source>
-        <translation>Game over!</translation>
+      <location filename="../../src/ui/MainWindow.cpp" line="192"/>
+      <source>Игра окончена!</source>
+      <translation>Game over!</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>PlayerNameDialog</name>
     <message>
-        <location filename="../../src/ui/PlayerNameDialog.cpp" line="11"/>
-        <source>Введите имена игроков</source>
-        <translation>Enter the names of the players</translation>
+      <location filename="../../src/ui/PlayerNameDialog.cpp" line="11"/>
+      <source>Введите имена игроков</source>
+      <translation>Enter the names of the players</translation>
     </message>
     <message>
-        <location filename="../../src/ui/PlayerNameDialog.cpp" line="22"/>
-        <source>Игрок %1:</source>
-        <translation>Player %1:</translation>
+      <location filename="../../src/ui/PlayerNameDialog.cpp" line="22"/>
+      <source>Игрок %1:</source>
+      <translation>Player %1:</translation>
     </message>
     <message>
-        <location filename="../../src/ui/PlayerNameDialog.cpp" line="26"/>
-        <source>Начать игру</source>
-        <translation>Start the game</translation>
+      <location filename="../../src/ui/PlayerNameDialog.cpp" line="26"/>
+      <source>Начать игру</source>
+      <translation>Start the game</translation>
     </message>
     <message>
-        <location filename="../../src/ui/PlayerNameDialog.cpp" line="27"/>
-        <source>Назад</source>
-        <translation>Back</translation>
+      <location filename="../../src/ui/PlayerNameDialog.cpp" line="27"/>
+      <source>Назад</source>
+      <translation>Back</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>SettingsDialog</name>
     <message>
-        <location filename="../../src/ui/SettingsDialog.cpp" line="24"/>
-        <source>Настройки</source>
-        <translation>Settings</translation>
+      <location filename="../../src/ui/SettingsDialog.cpp" line="24"/>
+      <source>Настройки</source>
+      <translation>Settings</translation>
     </message>
     <message>
-        <location filename="../../src/ui/SettingsDialog.cpp" line="30"/>
-        <source>Русский</source>
-        <translation>Русский</translation>
+      <location filename="../../src/ui/SettingsDialog.cpp" line="30"/>
+      <source>Русский</source>
+      <translation>Русский</translation>
     </message>
     <message>
-        <location filename="../../src/ui/SettingsDialog.cpp" line="31"/>
-        <source>English</source>
-        <translation>English</translation>
+      <location filename="../../src/ui/SettingsDialog.cpp" line="31"/>
+      <source>English</source>
+      <translation>English</translation>
     </message>
     <message>
-        <location filename="../../src/ui/SettingsDialog.cpp" line="32"/>
-        <source>Беларуская</source>
-        <translation>Беларуская</translation>
+      <location filename="../../src/ui/SettingsDialog.cpp" line="32"/>
+      <source>Беларуская</source>
+      <translation>Беларуская</translation>
     </message>
     <message>
-        <location filename="../../src/ui/SettingsDialog.cpp" line="39"/>
-        <source>Выбрать цвет...</source>
-        <translation>Set color...</translation>
+      <location filename="../../src/ui/SettingsDialog.cpp" line="39"/>
+      <source>Выбрать цвет...</source>
+      <translation>Set color...</translation>
     </message>
     <message>
-        <location filename="../../src/ui/SettingsDialog.cpp" line="47"/>
-        <source>Язык (требует перезапуска):</source>
-        <translation>Language (requires restart):</translation>
+      <location filename="../../src/ui/SettingsDialog.cpp" line="47"/>
+      <source>Язык (требует перезапуска):</source>
+      <translation>Language (requires restart):</translation>
     </message>
     <message>
-        <location filename="../../src/ui/SettingsDialog.cpp" line="52"/>
-        <source>Громкость:</source>
-        <translation>Volume:</translation>
+      <location filename="../../src/ui/SettingsDialog.cpp" line="52"/>
+      <source>Громкость:</source>
+      <translation>Volume:</translation>
     </message>
     <message>
-        <location filename="../../src/ui/SettingsDialog.cpp" line="53"/>
-        <source>Цвет фона:</source>
-        <translation>Background color:</translation>
+      <location filename="../../src/ui/SettingsDialog.cpp" line="53"/>
+      <source>Цвет фона:</source>
+      <translation>Background color:</translation>
     </message>
     <message>
-        <location filename="../../src/ui/SettingsDialog.cpp" line="91"/>
-        <source>Смена языка</source>
-        <translation>Change Language</translation>
+      <location filename="../../src/ui/SettingsDialog.cpp" line="91"/>
+      <source>Смена языка</source>
+      <translation>Change Language</translation>
     </message>
     <message>
-        <location filename="../../src/ui/SettingsDialog.cpp" line="91"/>
-        <source>Язык будет изменен после перезапуска приложения.</source>
-        <translation>The language will be changed after restarting the application.</translation>
+      <location filename="../../src/ui/SettingsDialog.cpp" line="91"/>
+      <source>Язык будет изменен после перезапуска приложения.</source>
+      <translation>The language will be changed after restarting the application.</translation>
     </message>
     <message>
-        <location filename="../../src/ui/SettingsDialog.cpp" line="98"/>
-        <source>Выбор цвет фона</source>
-        <translation>Choose background color</translation>
+      <location filename="../../src/ui/SettingsDialog.cpp" line="98"/>
+      <source>Выбор цвет фона</source>
+      <translation>Choose background color</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>StartWindow</name>
     <message>
-        <location filename="../../src/ui/StartWindow.cpp" line="20"/>
-        <source>Начать</source>
-        <translation>Start</translation>
+      <location filename="../../src/ui/StartWindow.cpp" line="20"/>
+      <source>Начать</source>
+      <translation>Start</translation>
     </message>
     <message>
-        <location filename="../../src/ui/StartWindow.cpp" line="21"/>
-        <source>Настройки</source>
-        <translation>Settings</translation>
+      <location filename="../../src/ui/StartWindow.cpp" line="21"/>
+      <source>Настройки</source>
+      <translation>Settings</translation>
     </message>
     <message>
-        <location filename="../../src/ui/StartWindow.cpp" line="22"/>
-        <source>Редактирование</source>
-        <translation>Edit</translation>
+      <location filename="../../src/ui/StartWindow.cpp" line="22"/>
+      <source>Редактирование</source>
+      <translation>Edit</translation>
     </message>
     <message>
-        <location filename="../../src/ui/StartWindow.cpp" line="42"/>
-        <source>Своя Игра - Меню</source>
-        <translation>Jeopardy - Menu</translation>
+      <location filename="../../src/ui/StartWindow.cpp" line="42"/>
+      <source>Своя Игра - Меню</source>
+      <translation>Jeopardy - Menu</translation>
     </message>
     <message>
-        <location filename="../../src/ui/StartWindow.cpp" line="77"/>
-        <source>Авторизация</source>
-        <translation>Authentication</translation>
+      <location filename="../../src/ui/StartWindow.cpp" line="77"/>
+      <source>Авторизация</source>
+      <translation>Authentication</translation>
     </message>
     <message>
-        <location filename="../../src/ui/StartWindow.cpp" line="78"/>
-        <source>Введите пароль администратора:</source>
-        <translation>Enter administrator password:</translation>
+      <location filename="../../src/ui/StartWindow.cpp" line="78"/>
+      <source>Введите пароль администратора:</source>
+      <translation>Enter administrator password:</translation>
     </message>
     <message>
-        <location filename="../../src/ui/StartWindow.cpp" line="88"/>
-        <source>Ошибка</source>
-        <translation>Error</translation>
+      <location filename="../../src/ui/StartWindow.cpp" line="88"/>
+      <source>Ошибка</source>
+      <translation>Error</translation>
     </message>
     <message>
-        <location filename="../../src/ui/StartWindow.cpp" line="88"/>
-        <source>Неверный пароль!</source>
-        <translation>Password is incorrect!</translation>
+      <location filename="../../src/ui/StartWindow.cpp" line="88"/>
+      <source>Неверный пароль!</source>
+      <translation>Password is incorrect!</translation>
     </message>
-</context>
+  </context>
 </TS>
