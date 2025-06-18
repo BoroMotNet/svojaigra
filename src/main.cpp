ed18b7d94cf8dacac4e254834fc358a2ff3e3284
Matvey Natsevskiy <matvey.natsevskiy@gmail.com>
2025-06-18 03:49:18 +0300

    translated debug

--- src/main.cpp
+++ src/main.cpp
@@ -17,43 +17,44 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName("Borovik Ivan T-391");
     QCoreApplication::setApplicationName("Своя Игра");
 
-    qDebug() << "--- ПАЧАТАК ДЫЯГНОСТЫКІ ЛАКАЛІЗАЦЫІ ---";
+    qDebug() << "--- НАЧАЛО ДИАГНОСТИКИ ЛОКАЛИЗАЦИИ ---";
 
     QString lang = FileManager::loadLanguageSetting();
-    qDebug() << "[INFO] Мова, загружаная з налад:" << lang;
+    qDebug() << "[INFO] Язык, загруженный из настроек:" << lang;
 
     QString workingDir = QCoreApplication::applicationDirPath();
-    qDebug() << "[INFO] Рабочая дырэкторыя праграмы:" << workingDir;
+    qDebug() << "[INFO] Рабочая директория приложения:" << workingDir;
 
     QString translationsPath = workingDir + "/translations";
-    qDebug() << "[CHECK] Поўны шлях да папкі перакладаў:" << translationsPath;
+    qDebug() << "[CHECK] Полный путь к папке переводов:" << translationsPath;
 
     QDir translationsDir(translationsPath);
     if (translationsDir.exists()) {
-        qDebug() << "[OK] Папка 'translations' існуе.";
+        qDebug() << "[OK] Папка 'translations' существует.";
     } else {
-        qWarning() << "[ERROR] Папка 'translations' НЕ ІСНУЕ па ўказаным шляху!";
+        qWarning() << "[ERROR] Папка 'translations' НЕ СУЩЕСТВУЕТ по указанному пути!";
     }
 
     QString qmFileName = "svojaigra_" + lang + ".qm";
     QString fullQmPath = translationsPath + "/" + qmFileName;
-    qDebug() << "[CHECK] Поўны шлях да файла перакладу:" << fullQmPath;
+    qDebug() << "[CHECK] Полный путь к файлу перевода:" << fullQmPath;
 
     if (QFile::exists(fullQmPath)) {
-        qDebug() << "[OK] Файл" << qmFileName << "існуе.";
+        qDebug() << "[OK] Файл" << qmFileName << "существует.";
     } else {
-        qWarning() << QStringLiteral("[ERROR] Файл \"%1\" НЕ ЗНОЙДЗЕНЫ па ўказаным шляху!").arg(qmFileName);
+        qWarning() << QStringLiteral("[ERROR] Файл \"%1\" НЕ НАЙДЕН по указанному пути!").arg(qmFileName);
     }
 
     QTranslator appTranslator;
     if (appTranslator.load("svojaigra_" + lang, translationsPath)) {
         app.installTranslator(&appTranslator);
-        qDebug() << "[OK] Файл перакладу паспяхова загружаны з дыска!";
+        qDebug() << "[OK] Файл перевода успешно загружен с диска!";
     } else {
-        qWarning() << "[ERROR] Спроба загрузкі файла не атрымалася!";
+        qWarning() << "[ERROR] Не удалось загрузить файл перевода!";
     }
 
-    qDebug() << "--- КАНЕЦ ДЫЯГНОСТЫКІ ЛАКАЛІЗАЦЫІ ---";
+    qDebug() << "--- КОНЕЦ ДИАГНОСТИКИ ЛОКАЛИЗАЦИИ ---";
+
 
     QTranslator qtTranslator;
     #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
