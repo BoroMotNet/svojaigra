14071cd15f265e2dd9f346905948d8f73600efbe
Матвей Нацевский <matvey.natsevskiy@gmail.com>
2025-06-23 01:00:44 +0300

    New translations svojaigra_ru.ts (Belarusian)

--- resources/translations/svojaigra_be.ts
+++ resources/translations/svojaigra_be.ts
@@ -21,7 +21,7 @@
     <message>
       <location filename="../../src/ui/AdminEditor.cpp" line="103"/>
       <source> Добавить</source>
-      <translation type="unfinished"> Добавить</translation>
+      <translation> Дадаць</translation>
     </message>
     <message>
       <location filename="../../src/ui/AdminEditor.cpp" line="136"/>
@@ -110,7 +110,7 @@
     <message>
       <location filename="../../src/core/GameManager.cpp" line="142"/>
       <source>&apos;, Очки: </source>
-      <translation type="unfinished">&apos;, Очки: </translation>
+      <translation>&apos;, Ачкі: </translation>
     </message>
     <message>
       <location filename="../../src/core/GameManager.cpp" line="102"/>
