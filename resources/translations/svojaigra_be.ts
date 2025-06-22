6bf98e5bf7827d68aadc26cde2845b15cf0d98fc
Матвей Нацевский <matvey.natsevskiy@gmail.com>
2025-06-22 22:43:43 +0300

    New translations svojaigra_ru.ts (Belarusian)

--- resources/translations/svojaigra_be.ts
+++ resources/translations/svojaigra_be.ts
@@ -6,7 +6,7 @@
     <message>
       <location filename="../../src/ui/AdminEditor.cpp" line="10"/>
       <source>Редактор вопросов</source>
-      <translation>Рэдактар ​​пытанняў</translation>
+      <translation type="unfinished">Редактор вопросов</translation>
     </message>
   </context>
   <context>
@@ -14,32 +14,32 @@
     <message>
       <location filename="../../src/core/GameManager.cpp" line="66"/>
       <source>Не удалось загрузить вопросы. Игра не может быть начата.</source>
-      <translation>Не ўдалося загрузіць пытанні. Гульня не можа быць пачата.</translation>
+      <translation type="unfinished">Не удалось загрузить вопросы. Игра не может быть начата.</translation>
     </message>
     <message>
       <location filename="../../src/core/GameManager.cpp" line="92"/>
       <source>Игрок</source>
-      <translation>Гулец</translation>
+      <translation type="unfinished">Игрок</translation>
     </message>
     <message>
       <location filename="../../src/core/GameManager.cpp" line="93"/>
       <source>не может выбрать этот вопрос (порог сложности)!</source>
-      <translation>не можа выбраць гэтае пытанне (парог складанасці)!</translation>
+      <translation type="unfinished">не может выбрать этот вопрос (порог сложности)!</translation>
     </message>
     <message>
       <location filename="../../src/core/GameManager.cpp" line="101"/>
       <source>Попытка выбрать уже сыгранный вопрос!</source>
-      <translation>Спроба выбраць ужо згулянае пытанне!</translation>
+      <translation type="unfinished">Попытка выбрать уже сыгранный вопрос!</translation>
     </message>
     <message>
       <location filename="../../src/core/GameManager.cpp" line="128"/>
       <source>Порог для игрока</source>
-      <translation>Парог для гульца</translation>
+      <translation type="unfinished">Порог для игрока</translation>
     </message>
     <message>
       <location filename="../../src/core/GameManager.cpp" line="128"/>
       <source>повышен до</source>
-      <translation>павышаны да</translation>
+      <translation type="unfinished">повышен до</translation>
     </message>
   </context>
   <context>
@@ -47,12 +47,12 @@
     <message>
       <location filename="../../src/ui/MainWindow.cpp" line="30"/>
       <source>Своя Игра</source>
-      <translation>Свая Гульня</translation>
+      <translation type="unfinished">Своя Игра</translation>
     </message>
     <message>
       <location filename="../../src/ui/MainWindow.cpp" line="192"/>
       <source>Игра окончена!</source>
-      <translation>Гульня скончана!</translation>
+      <translation type="unfinished">Игра окончена!</translation>
     </message>
   </context>
   <context>
@@ -60,22 +60,22 @@
     <message>
       <location filename="../../src/ui/PlayerNameDialog.cpp" line="11"/>
       <source>Введите имена игроков</source>
-      <translation>Увядзіце імёны гульцоў</translation>
+      <translation type="unfinished">Введите имена игроков</translation>
     </message>
     <message>
       <location filename="../../src/ui/PlayerNameDialog.cpp" line="22"/>
       <source>Игрок %1:</source>
-      <translation>Гулец %1:</translation>
+      <translation type="unfinished">Игрок %1:</translation>
     </message>
     <message>
       <location filename="../../src/ui/PlayerNameDialog.cpp" line="26"/>
       <source>Начать игру</source>
-      <translation>Пачаць гульню</translation>
+      <translation type="unfinished">Начать игру</translation>
     </message>
     <message>
       <location filename="../../src/ui/PlayerNameDialog.cpp" line="27"/>
       <source>Назад</source>
-      <translation>Назад</translation>
+      <translation type="unfinished">Назад</translation>
     </message>
   </context>
   <context>
@@ -83,67 +83,67 @@
     <message>
       <location filename="../../src/ui/SettingsDialog.cpp" line="36"/>
       <source>Настройки</source>
-      <translation>Налады</translation>
+      <translation type="unfinished">Настройки</translation>
     </message>
     <message>
       <location filename="../../src/ui/SettingsDialog.cpp" line="21"/>
       <source>Русский</source>
-      <translation>Русский</translation>
+      <translation type="unfinished">Русский</translation>
     </message>
     <message>
       <location filename="../../src/ui/SettingsDialog.cpp" line="22"/>
       <source>English</source>
-      <translation>English</translation>
+      <translation type="unfinished">English</translation>
     </message>
     <message>
       <location filename="../../src/ui/SettingsDialog.cpp" line="23"/>
       <source>Беларуская</source>
-      <translation>Беларуская</translation>
+      <translation type="unfinished">Беларуская</translation>
     </message>
     <message>
       <location filename="../../src/ui/SettingsDialog.ui" line="14"/>
       <source>Своя Игра - Настройки</source>
-      <translation>Свая Гульня - Налады</translation>
+      <translation type="unfinished">Своя Игра - Настройки</translation>
     </message>
     <message>
       <location filename="../../src/ui/SettingsDialog.ui" line="33"/>
       <source>Язык</source>
-      <translation>Мова</translation>
+      <translation type="unfinished">Язык</translation>
     </message>
     <message>
       <location filename="../../src/ui/SettingsDialog.ui" line="47"/>
       <source>Громкость</source>
-      <translation>Гучнасць</translation>
+      <translation type="unfinished">Громкость</translation>
     </message>
     <message>
       <location filename="../../src/ui/SettingsDialog.ui" line="64"/>
       <source>100%</source>
-      <translation>100%</translation>
+      <translation type="unfinished">100%</translation>
     </message>
     <message>
       <location filename="../../src/ui/SettingsDialog.ui" line="75"/>
       <source>Цвет</source>
-      <translation>Колер</translation>
+      <translation type="unfinished">Цвет</translation>
     </message>
     <message>
       <location filename="../../src/ui/SettingsDialog.ui" line="82"/>
       <source>Выбрать цвет...</source>
-      <translation>Выбраць колер...</translation>
+      <translation type="unfinished">Выбрать цвет...</translation>
     </message>
     <message>
       <location filename="../../src/ui/SettingsDialog.cpp" line="73"/>
       <source>Смена языка</source>
-      <translation>Пераключыць мову</translation>
+      <translation type="unfinished">Смена языка</translation>
     </message>
     <message>
       <location filename="../../src/ui/SettingsDialog.cpp" line="73"/>
       <source>Язык будет изменен после перезапуска приложения.</source>
-      <translation>Мова будзе зменена пасля перазапуску прыкладання.</translation>
+      <translation type="unfinished">Язык будет изменен после перезапуска приложения.</translation>
     </message>
     <message>
       <location filename="../../src/ui/SettingsDialog.cpp" line="79"/>
       <source>Выбор цвет фона</source>
-      <translation>Выбар колеру фону</translation>
+      <translation type="unfinished">Выбор цвет фона</translation>
     </message>
   </context>
   <context>
@@ -152,57 +152,57 @@
       <location filename="../../src/ui/StartWindow.ui" line="14"/>
       <location filename="../../src/ui/StartWindow.ui" line="59"/>
       <source>Своя Игра</source>
-      <translation>Свая Гульня</translation>
+      <translation type="unfinished">Своя Игра</translation>
     </message>
     <message>
       <location filename="../../src/ui/StartWindow.ui" line="74"/>
       <source>Иван Боровик, Т-391</source>
-      <translation>Іван Баравік, Т-391</translation>
+      <translation type="unfinished">Иван Боровик, Т-391</translation>
     </message>
     <message>
       <location filename="../../src/ui/StartWindow.ui" line="111"/>
       <source>Начать</source>
-      <translation>Пачаць</translation>
+      <translation type="unfinished">Начать</translation>
     </message>
     <message>
       <location filename="../../src/ui/StartWindow.ui" line="129"/>
       <source>Настройки</source>
-      <translation>Налады</translation>
+      <translation type="unfinished">Настройки</translation>
     </message>
     <message>
       <location filename="../../src/ui/StartWindow.ui" line="147"/>
       <source>Редактирование</source>
-      <translation>Рэдагаванне</translation>
+      <translation type="unfinished">Редактирование</translation>
     </message>
     <message>
       <location filename="../../src/ui/StartWindow.ui" line="165"/>
       <source>Выход</source>
-      <translation>Выхад</translation>
+      <translation type="unfinished">Выход</translation>
     </message>
     <message>
       <location filename="../../src/ui/StartWindow.cpp" line="27"/>
       <source>Своя Игра - Меню</source>
-      <translation>Свая Гульня - Меню</translation>
+      <translation type="unfinished">Своя Игра - Меню</translation>
     </message>
     <message>
       <location filename="../../src/ui/StartWindow.cpp" line="62"/>
       <source>Авторизация</source>
-      <translation>Аўтарызацыя</translation>
+      <translation type="unfinished">Авторизация</translation>
     </message>
     <message>
       <location filename="../../src/ui/StartWindow.cpp" line="63"/>
       <source>Введите пароль администратора:</source>
-      <translation>Увядзіце пароль адміністратара:</translation>
+      <translation type="unfinished">Введите пароль администратора:</translation>
     </message>
     <message>
       <location filename="../../src/ui/StartWindow.cpp" line="73"/>
       <source>Ошибка</source>
-      <translation>Памылка</translation>
+      <translation type="unfinished">Ошибка</translation>
     </message>
     <message>
       <location filename="../../src/ui/StartWindow.cpp" line="73"/>
       <source>Неверный пароль!</source>
-      <translation>Няправільны пароль!</translation>
+      <translation type="unfinished">Неверный пароль!</translation>
     </message>
   </context>
 </TS>
