cc0c236fd26893ea222010682b0159c48932f5a9
Матвей Нацевский <matvey.natsevskiy@gmail.com>
2025-06-26 03:14:38 +0300

    New translations svojaigra_ru.ts (English)

--- resources/translations/svojaigra_en.ts
+++ resources/translations/svojaigra_en.ts
@@ -16,12 +16,12 @@
     <message>
       <location filename="../../src/ui/AdminEditor.cpp" line="45"/>
       <source>Сохранить все изменения</source>
-      <translation type="unfinished">Сохранить все изменения</translation>
+      <translation>Save All Changes</translation>
     </message>
     <message>
       <location filename="../../src/ui/AdminEditor.cpp" line="49"/>
       <source>Выход в главное меню</source>
-      <translation type="unfinished">Выход в главное меню</translation>
+      <translation>Exit to Main Menu</translation>
     </message>
     <message>
       <location filename="../../src/ui/AdminEditor.cpp" line="105"/>
@@ -82,14 +82,14 @@
     <message>
       <location filename="../../src/ui/AdminEditor.cpp" line="212"/>
       <source>Несохраненные изменения</source>
-      <translation type="unfinished">Несохраненные изменения</translation>
+      <translation>Unsaved Changes</translation>
     </message>
     <message>
       <location filename="../../src/ui/AdminEditor.cpp" line="213"/>
       <source>У вас есть несохраненные изменения. Вы уверены, что хотите выйти?
 Все несохраненные данные будут потеряны.</source>
-      <translation type="unfinished">У вас есть несохраненные изменения. Вы уверены, что хотите выйти?
-Все несохраненные данные будут потеряны.</translation>
+      <translation>You have unsaved changes. Are you sure you want to exit?
+All unsaved data will be lost.</translation>
     </message>
   </context>
   <context>
@@ -102,12 +102,12 @@
     <message>
       <location filename="../../src/core/GameManager.cpp" line="63"/>
       <source>Ошибка загрузки</source>
-      <translation type="unfinished">Ошибка загрузки</translation>
+      <translation>Download Failed</translation>
     </message>
     <message>
       <location filename="../../src/core/GameManager.cpp" line="64"/>
       <source>Не удалось загрузить вопросы. Проверьте файлы игры.</source>
-      <translation type="unfinished">Не удалось загрузить вопросы. Проверьте файлы игры.</translation>
+      <translation>Failed to load questions. Check game files.</translation>
     </message>
     <message>
       <location filename="../../src/core/GameManager.cpp" line="122"/>
@@ -368,7 +368,7 @@
     <message>
       <location filename="../../src/ui/StartWindow.cpp" line="37"/>
       <source>Введите хотя бы одно имя игрока!</source>
-      <translation type="unfinished">Введите хотя бы одно имя игрока!</translation>
+      <translation>Please enter at least one player name!</translation>
     </message>
     <message>
       <location filename="../../src/ui/StartWindow.cpp" line="62"/>
